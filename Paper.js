99f5a179d34ec9f1eb315cecad02efd67be9d57e
NayanPRaj <74858861+NayanPRaj@users.noreply.github.com>
2021-01-05 18:16:37 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,25 @@
+class Paper {  
+    constructor(x, y, radius){
+        var options = {
+            'restitution':0.6,
+            'friction':1.0,
+            'density':1.0,
+            'isStatic' : false
+        }
+         this.radius = radius
+        this.body = Bodies.circle(x, y, radius, options);
+        World.add(world, this.body);
+        this.image=loadImage("paper.png");
+       
+        
+   }
+   display(){
+    fill("white");
+    //ellipseMode(RADIUS);
+    //ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+    image(this.image,this.body.position.x,this.body.position.y,50,50);
+
+   }
+}
+
+
